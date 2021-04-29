898834954289b0f823f0c7bea986d56cbe8e9a42
Lukasz <janouinteria@gmail.com>
2021-04-29 12:39:22 +0200

    deleted secKey

--- electronic_shop/settings.py
+++ electronic_shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l!sem@qglnpl*wdmu--2=_)jyzye5-n!czbtzu45o2#v27*yh4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
