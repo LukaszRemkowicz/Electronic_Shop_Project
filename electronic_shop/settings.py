d138f9024f2f737a28482fe1d05850825beadcc5
Lukasz <janouinteria@gmail.com>
2021-05-03 11:33:23 +0200

    added base.html

--- electronic_shop/settings.py
+++ electronic_shop/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'electronic_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,6 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "electronic_statics")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- electronic_shop/urls.py
+++ electronic_shop/urls.py
@@ -7,5 +7,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Profile.urls")),
-    path('', TemplateView.as_view(template_name='index.html'), name="landing-page")
+    path('', TemplateView.as_view(template_name='landing_page.html'), name="landing-page")
 ]
