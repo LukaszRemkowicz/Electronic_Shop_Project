a7e5b02d3d47a95f04196f065e72dc2ae49c13cc
janouinteria@gmail.com <janouinteria@gmail.com>
2022-04-01 00:37:50 +0200

    Changed docker config. Fixed doubled variable in mediaQuery.js. Updated requirements

--- electronic_shop/settings.py
+++ electronic_shop/settings.py
@@ -197,9 +197,9 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
-def show_toolbar(request):
-    return True
+# def show_toolbar(request):
+#     return True
 
-DEBUG_TOOLBAR_CONFIG = {
-  "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
-}
+# DEBUG_TOOLBAR_CONFIG = {
+#   "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+# }
